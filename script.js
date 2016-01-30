db6e9ba7b808d97cdd2953c328721d16e2c41b2f
Shaggy8 <rominus@bigmir.net>
2016-01-30 20:05:57 +0200

    Signed-off-by: Shaggy8 <rominus@bigmir.net>

---
+++ script.js
@@ -0,0 +1,12 @@
+var table = document.getElementsByTagName('table')[0];
+var mainArr = [];
+
+for (var i = 1; i < table.rows.length; i++) {
+  var tempArr = [];
+  for (var k = 0; k < table.rows[i].cells.length; k++) {
+    tempArr.push(table.rows[i].cells[k].textContent);
+  }
+  mainArr.push(tempArr);
+}
+
+alert(mainArr.length)
